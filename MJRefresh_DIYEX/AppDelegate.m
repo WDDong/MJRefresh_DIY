70faa7a172b00264179969351f6c1a787c90ba82
wangdongdong <meng0928dong@163.com>
2019-10-29 14:32:31 +0800

    Update AppDelegate.m

--- MJRefresh_DIYEX/AppDelegate.m
+++ MJRefresh_DIYEX/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"3331");
-    NSLog(@"11111");
+    NSLog(@"111112");
     return YES;
 }
 
