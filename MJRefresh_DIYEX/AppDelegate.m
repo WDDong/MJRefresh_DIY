6974955f64341eafbfc3814ba57ed0b2d1ecbead
dong <>
2019-10-29 14:15:21 +0800

    no message

--- MJRefresh_DIYEX/AppDelegate.m
+++ MJRefresh_DIYEX/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"3331");
+    NSLog(@"11111");
     return YES;
 }
 
